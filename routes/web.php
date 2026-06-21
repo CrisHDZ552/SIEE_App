f4bd72f0a281d713523aaebf061b14fd34029794
Gerardo <gera_2281@outlook.com>
2026-06-20 22:20:16 -0600

    Rutas crear y agregar carpetas

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\EscuelaController;
 
 Route::get('/', [EscuelaController::class, 'index']);
 Route::get('/escuelas/search', [EscuelaController::class, 'search'])->name('escuelas.search');
+Route::get('/crear-carpetas-escuelas', [EscuelaController::class, 'CrearCarpetasEscuelas'])->name('escuelas.creaRR');
+Route::post('/agregar-carpetas-escuelas', [EscuelaController::class, 'ValidarCarpetasEscuelas'])->name('escuelas.agg');
+
 
 
 
