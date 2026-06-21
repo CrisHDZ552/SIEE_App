8354d22856ad836ca8f05d5676b9c5f11843f10b
Gerardo <gera_2281@outlook.com>
2026-06-20 22:52:27 -0600

    Cambio nombre rutas

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\EscuelaController;
 
 Route::get('/', [EscuelaController::class, 'index']);
 Route::get('/escuelas/search', [EscuelaController::class, 'search'])->name('escuelas.search');
-Route::get('/crear-carpetas-escuelas', [EscuelaController::class, 'CrearCarpetasEscuelas'])->name('escuelas.creaRR');
-Route::post('/agregar-carpetas-escuelas', [EscuelaController::class, 'ValidarCarpetasEscuelas'])->name('escuelas.agg');
+Route::get('/crear/carpetas', [EscuelaController::class, 'CrearCarpetasEscuelas'])->name('escuelas.creaRR');
+Route::post('/agregar/carpetas', [EscuelaController::class, 'ValidarCarpetasEscuelas'])->name('escuelas.agg');
 
 
 
