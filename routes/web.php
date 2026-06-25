fb60333f224aa7aabfb7af75063a968ab8bebd40
Gerardo <gera_2281@outlook.com>
2026-06-25 12:53:42 -0600

    rutas nuevas

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::get('/escuelas/search', [EscuelaController::class, 'search'])->name('escu
 Route::get('/crear/carpetas', [EscuelaController::class, 'CrearCarpetasEscuelas'])->name('escuelas.creaRR');
 Route::post('/agregar/carpetas', [EscuelaController::class, 'ValidarCarpetasEscuelas'])->name('escuelas.agg');
 Route::get('/escuelas/{escuela}', [EscuelaController::class, 'show'])->name('escuelas.show');
+Route::get('/crear/archivos', [EscuelaController::class, 'CrearArchivosEscuelas'])->name('archivos.creaRR');
+Route::post('/agregar/archivos', [EscuelaController::class, 'ValidarArchivosEscuelas'])->name('archivos.agg');
