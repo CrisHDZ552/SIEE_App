6cfe246e3238dc3de8c522c1e0c26dd8813475f9
Gerardo <gera_2281@outlook.com>
2026-06-25 12:54:30 -0600

    funcion validar y crear archivos

--- app/Http/Controllers/EscuelaController.php
+++ app/Http/Controllers/EscuelaController.php
@@ -68,6 +68,33 @@ class EscuelaController extends Controller
         return redirect('/');
     }
 
+    public function CrearArchivosEscuelas()
+    {
+        return view('Archivos.CrearA');
+    }
+
+    public function ValidarArchivosEscuelas(Request $request)
+    {
+        $request->validate([     //Validar que los campos no esten vacios y max caracteres
+            'numero_escuela' => 'required|max:15|unique:escuelas,numero_escuela',
+            'ctt' => 'required|max:15',
+        ]);
+
+        $escuela = new Escuela();
+        $escuela->numero_escuela = $request->numero_escuela; //Darle valor al objeto
+        $escuela->ctt = $request->ctt;
+        //$escuela->user_id = Auth::id();  //Guardar el usuario que creo la carpeta
+        $escuela->save();
+
+        // Crear la carpeta en public/archivos
+        $folderName = $escuela->numero_escuela . '_' . uniqid();
+        $path = public_path('archivos/' . $folderName);
+
+        File::makeDirectory($path, 0755, true, true);
+
+        return redirect('escuelas.show');
+    }
+
     public function show(Escuela $escuela)
     {
         // Obtener la carpeta de la escuela en public/archivos
