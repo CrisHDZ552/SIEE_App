e6ffca42a94b5e47c23d44abf72761fb558466af
Gerardo <gera_2281@outlook.com>
2026-06-25 22:19:55 -0600

    cambio nombre rutas y en base al id de escuela

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EscuelaController;
+use App\Http\Controllers\ArchivosController;
 
 Route::get('/', [EscuelaController::class, 'index']);
 Route::get('/escuelas/search', [EscuelaController::class, 'search'])->name('escuelas.search');
 Route::get('/crear/carpetas', [EscuelaController::class, 'CrearCarpetasEscuelas'])->name('escuelas.creaRR');
 Route::post('/agregar/carpetas', [EscuelaController::class, 'ValidarCarpetasEscuelas'])->name('escuelas.agg');
 Route::get('/escuelas/{escuela}', [EscuelaController::class, 'show'])->name('escuelas.show');
-Route::get('/crear/archivos', [EscuelaController::class, 'CrearArchivosEscuelas'])->name('archivos.creaRR');
-Route::post('/agregar/archivos', [EscuelaController::class, 'ValidarArchivosEscuelas'])->name('archivos.agg');
+Route::get('/crear/carpetasarchivos/{escuela}', [ArchivosController::class, 'CrearCarpetasA'])->name('archivos.creaRR');
+Route::post('/agregar/carpetasarchivos/{escuela}', [ArchivosController::class, 'ValidarCarpetasA'])->name('archivos.agg');
