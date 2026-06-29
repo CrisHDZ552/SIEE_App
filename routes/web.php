deeda5ab6f2530e40046c69ad556b22c0bc1a98f
Gerardo <gera_2281@outlook.com>
2026-06-28 21:17:03 -0600

    para que no te pierdas en las rutas

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,24 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EscuelaController;
 use App\Http\Controllers\ArchivosController;
 
+//inicio de la aplicacion y visualizacion de escuelas
 Route::get('/', [EscuelaController::class, 'index']);
+//Rutas para buscar escuelas
 Route::get('/escuelas/search', [EscuelaController::class, 'search'])->name('escuelas.search');
+
+//Rutas para agregar y validar carpetas de escuelas
 Route::get('/crear/carpetas', [EscuelaController::class, 'CrearCarpetasEscuelas'])->name('escuelas.creaRR');
 Route::post('/agregar/carpetas', [EscuelaController::class, 'ValidarCarpetasEscuelas'])->name('escuelas.agg');
+//Ruta para mostrar la vista lo que hay dentro de la carpeta escuela (Carpetas, Archivos)
 Route::get('/escuelas/{escuela}', [EscuelaController::class, 'show'])->name('escuelas.show');
+
+//Rutas para agregar y validar carpetas dentro de las carpetas de escuelas
 Route::get('/crear/carpetasarchivos/{escuela}', [ArchivosController::class, 'CrearCarpetasA'])->name('archivos.creaRR');
 Route::post('/agregar/carpetasarchivos/{escuela}', [ArchivosController::class, 'ValidarCarpetasA'])->name('archivos.agg');
-Route::get('/archivos/{escuela}', [ArchivosController::class, 'show'])->name('archivos.show');
-Route::get('/archivos/{escuela}/carpeta/{carpeta}', [ArchivosController::class, 'showCarpeta'])->name('archivos.carpeta');
+//Rutas para mostrar la vista de lo que hay dentro de estas carpetas de arriba ↑
+Route::get('/archivos/{escuela}/carpeta/{carpeta}', [ArchivosController::class, 'show'])->name('archivos.carpeta');
+
+//Rutas para Agregar y validar archivos
+Route::get('/crear/archivos/{escuela}', [ArchivosController::class, 'AggArchivos'])->name('archivos.crearArch');
+Route::post('/agregar/archivos/{escuela}', [ArchivosController::class, 'ValidarArchivos'])->name('archivos.agregarArch');
+
