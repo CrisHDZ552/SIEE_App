18b698fe37c6e68155cfde9162ced40a259a7cf8
Gerardo <gera_2281@outlook.com>
2026-06-28 21:15:20 -0600

    agregar el campo al modelo archivo

--- app/Models/Archivo.php
+++ app/Models/Archivo.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-#[Fillable(['nombre_carpeta', 'contenido'])]
+#[Fillable(['nombre_carpeta', 'contenido', 'imagen'])]
 class Archivo extends Model
 {
     use HasFactory;
